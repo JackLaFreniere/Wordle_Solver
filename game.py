a0dc7a1a7e6053dd8c8c5a8d7384d3b456b1c229
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2026-05-17 12:42:07 -0500

    Fixed bug with the normal game not ending.

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ class Wordle:
         self.attempts += 1
 
         if word == self.answer: #Player got it right
+            self.attempts = self.max_attempts
             return "g" * 5
         
         return self.get_byg(self.answer, word)
