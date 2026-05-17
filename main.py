b62456a8ba1f7e6b10d927e1bd65b2ee8860722f
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2026-05-17 15:17:34 -0500

    Added time debugging for how long the entire process takes

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import bot, word_helper, compute_data
+import bot, word_helper, compute_data, time
 from statistics import mean, median, mode
 
 jarvis = bot.Jarvis(compute_data.get_pattern_table(), compute_data.get_best_guesses())
 all_possible_answers = word_helper.get_possible_answers()
 
+start_time = time.time()
 scores = []
 # print(jarvis.play_game("creed"))
 for wordle_answer in all_possible_answers:
     result = jarvis.play_game(wordle_answer)
     scores.append(result)
     print(result)
+end_time = time.time()
+mins, secs = divmod(end_time - start_time, 60)
 
 #Prints analysis
 fails = []
@@ -28,7 +31,9 @@ total_attempts = len(guesses)
 total_successes = successes.count(True)
 success_rate = float(total_successes)/total_attempts
 
-print(f"\nTotal attempts: {total_attempts}")
+
+print(f"\nTotal time taken: {int(mins)}m:{int(secs)}s")
+print(f"Total attempts: {total_attempts}")
 print(f"Number of successes: {total_successes}")
 print(f"Success rate: {success_rate * 100:.5f}%")
 print(f"Average number of attempts: {mean(guesses):.5f}\n")
