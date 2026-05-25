1f5203b2cd9d29717812f9463eb2578c62804da1
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2026-05-25 10:53:18 -0500

    Added printing results to a file

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import word_helper, compute_data, time
 from wordle_solver_bot import Jarvis
 from statistics import mean
 
-def print_result(result:list):
+def print_result(result:list, file=None):
     guesses, responses, word, passed = result[0], result[1], result[2], result[3]
     print(f"{str(guesses):<54} {str(responses):<54} {word} {passed}")
+    print(f"{str(guesses):<54} {str(responses):<54} {word} {passed}", file=file)
 
-def play_all_games(bot:Jarvis, all_possible_answers:list) -> tuple[list, int, int]:
+def play_all_games(bot:Jarvis, all_possible_answers:list, file=None) -> tuple[list, int, int]:
     start_time = time.time()
 
     scores = []
     for wordle_answer in all_possible_answers:
         result = bot.play_game(wordle_answer)
         scores.append(result)
-        print_result(result)
+        print_result(result, file=file)
 
     end_time = time.time()
     mins, secs = divmod(end_time - start_time, 60)
@@ -27,7 +28,7 @@ def get_guess_distribution(guesses_list:list) -> dict:
 
     return guess_distribution
 
-def print_information_breakdown(scores:list, mins:int, secs:float):
+def print_information_breakdown(scores:list, mins:int, secs:float, file=None):
     #Prints analysis
     fails_list = []
     guesses_list = []
@@ -47,24 +48,26 @@ def print_information_breakdown(scores:list, mins:int, secs:float):
 
     guess_distribution = get_guess_distribution(guesses_list)
 
-    print(f"\nTotal time taken: {mins}m:{secs:.2f}s")
-    print(f"Total attempts: {total_attempts}")
-    print(f"Number of successes: {total_successes}")
-    print(f"Success rate: {success_rate * 100:.5f}%")
-    print(f"Average number of attempts: {mean(guesses_list):.5f}\n")
+    print(f"\nTotal time taken: {mins}m:{secs:.2f}s", file=file)
+    print(f"Total attempts: {total_attempts}", file=file)
+    print(f"Number of successes: {total_successes}", file=file)
+    print(f"Success rate: {success_rate * 100:.5f}%", file=file)
+    print(f"Average number of attempts: {mean(guesses_list):.5f}\n", file=file)
     
-    print(f"Guess distrubition:")
+    print(f"Guess distrubition:", file=file)
     for i in range(len(guess_distribution)):
-        print(f"{i + 1} guesses: {guess_distribution[i]:,} ({float(guess_distribution[i])/total_attempts * 100:.2f})%")
+        print(f"{i + 1} guesses: {guess_distribution[i]:,} ({float(guess_distribution[i])/total_attempts * 100:.2f})%", file=file)
 
-    print(f"Total fails: {len(fails_list)}")
+    print(f"Total fails: {len(fails_list)}", file=file)
     for fail in fails_list:
         print(fail)
 
 bot = Jarvis()
 all_possible_answers = word_helper.get_possible_answers_indexes()
 
-data = play_all_games(bot, all_possible_answers)
-results, mins, secs = data[0], data[1], data[2]
+with open("results.txt", "w") as file:
+    data = play_all_games(bot, all_possible_answers, file=file)
+    results, mins, secs = data[0], data[1], data[2]
 
-print_information_breakdown(results, mins, secs)
+    print_information_breakdown(results, mins, secs)
+    print_information_breakdown(results, mins, secs, file=file)
